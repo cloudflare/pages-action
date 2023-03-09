61eafe73baad0195ab582cb447b2c6e15a0df9ce
James Ross <james@jross.me>
2023-03-09 22:48:44 +0000

    Revert "Fix branch name for `pull_request` events" (#68)

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ try {
 		const deployment = await octokit.rest.repos.createDeployment({
 			owner: context.repo.owner,
 			repo: context.repo.repo,
-			ref: branch,
+			ref: context.ref,
 			auto_merge: false,
 			description: "Cloudflare Pages",
 			required_contexts: [],
@@ -125,7 +125,8 @@ try {
 		const project = await getProject();
 		if (!project) throw new Error("Unable to find pages project");
 
-		const productionEnvironment = branch === project.production_branch;
+		const githubBranch = env.GITHUB_REF_NAME;
+		const productionEnvironment = githubBranch === project.production_branch;
 		const environmentName = `${projectName} (${productionEnvironment ? "Production" : "Preview"})`;
 
 		let gitHubDeployment: Awaited<ReturnType<typeof createGitHubDeployment>>;
