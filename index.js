1476a552b4f7dd923596b9fe44e9df6e82a79e07
James Ross <james@jross.me>
2023-03-09 22:46:30 +0000

    Revert "Fix branch name for pull_request events (#56)"
    This reverts commit 0fc25c3c282fb26e9a53abfadf6d2898adcac3de.

--- index.js
+++ index.js
@@ -21114,12 +21114,12 @@ var __publicField2 = (obj, key, value) => {
 var Shell = class {
   constructor(env_passthrough = ["PATH"]) {
     __publicField(this, "process");
-    const env = { PS1: "" };
+    const env2 = { PS1: "" };
     env_passthrough.forEach((key) => {
-      env[key] = process.env[key];
+      env2[key] = process.env[key];
     });
     this.process = import_child_process.default.spawn("bash", ["--noprofile", "--norc"], {
-      env,
+      env: env2,
       detached: true
     });
     this.process.stdout.setEncoding("utf8");
@@ -22059,6 +22059,7 @@ var src_default = shellac;
 
 // src/index.ts
 var import_undici = __toESM(require_undici());
+var import_process = require("process");
 var import_node_path = __toESM(require("path"));
 try {
   const apiToken = (0, import_core.getInput)("apiToken", { required: true });
@@ -22098,7 +22099,7 @@ try {
     const deployment = await octokit.rest.repos.createDeployment({
       owner: import_github.context.repo.owner,
       repo: import_github.context.repo.repo,
-      ref: branch,
+      ref: import_github.context.ref,
       auto_merge: false,
       description: "Cloudflare Pages",
       required_contexts: [],
@@ -22155,7 +22156,8 @@ try {
     const project = await getProject();
     if (!project)
       throw new Error("Unable to find pages project");
-    const productionEnvironment = branch === project.production_branch;
+    const githubBranch = import_process.env.GITHUB_REF_NAME;
+    const productionEnvironment = githubBranch === project.production_branch;
     const environmentName = `${projectName} (${productionEnvironment ? "Production" : "Preview"})`;
     let gitHubDeployment;
     if (gitHubToken && gitHubToken.length) {
