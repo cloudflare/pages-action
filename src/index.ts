fd5884de2adf69987b63677f781de6d6f7482f84
Daniel Walsh <walshydev@gmail.com>
2023-05-21 09:36:23 +0100

    Better API errors

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,18 @@ try {
 			`https://api.cloudflare.com/client/v4/accounts/${accountId}/pages/projects/${projectName}`,
 			{ headers: { Authorization: `Bearer ${apiToken}` } }
 		);
+		if (response.status !== 200) {
+			console.error(`Cloudflare API returned non-200: ${response.status}`);
+			const json = await response.text();
+			console.error(`API returned: ${json}`);
+			throw new Error("Failed to get Pages project, API returned non-200");
+		}
+
 		const { result } = (await response.json()) as { result: Project | null };
+		if (result === null) {
+			throw new Error("Failed to get Pages project, project does not exist. Check the project name or create it!");
+		}
+
 		return result;
 	};
 
@@ -126,7 +137,6 @@ try {
 
 	(async () => {
 		const project = await getProject();
-		if (!project) throw new Error("Unable to find pages project");
 
 		const productionEnvironment = githubBranch === project.production_branch || branch === project.production_branch;
 		const environmentName = `${projectName} (${productionEnvironment ? "Production" : "Preview"})`;
